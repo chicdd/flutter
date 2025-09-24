e51084f11cebb0661f7cbf9bda1b1dd58c9f65d9
chicdd <chicdd02@gmail.com>
2025-09-24 16:11:20 +0900

    클로드 코드로 수정. kone, seodaemun, c1 개발환경 추가

--- neosecurity/ios/Runner/AppDelegate.swift
+++ neosecurity/ios/Runner/AppDelegate.swift
@@ -7,6 +7,31 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let configChannel = FlutterMethodChannel(name: "com.neo.config/channel",
+                                           binaryMessenger: controller.binaryMessenger)
+
+    configChannel.setMethodCallHandler({
+      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      switch call.method {
+      case "getAppName":
+        if let appName = Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String {
+          result(appName)
+        } else {
+          result("앱 이름 없음")
+        }
+      case "getGaetongCode":
+        if let gaetongCode = Bundle.main.infoDictionary?["FLUTTER_GAETONG_CODE"] as? String {
+          result(gaetongCode)
+        } else {
+          result("개통코드 없음")
+        }
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
