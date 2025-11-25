2105dae8acffc94d36b2fddfb0453ea72bf6af9e
chicdd <89671969+chicdd@users.noreply.github.com>
2025-11-25 11:36:18 +0900

    Delete securityindexAPI directory

--- securityindexAPI/Models/휴일주간.cs
+++
@@ -1,9 +0,0 @@
-namespace securityindexAPI.Models
-{
-    public class 휴일주간
-    {
-        public int 관리id { get; set; }
-        public required string 관제관리번호 { get; set; }
-        public required string 휴일주간코드 { get; set; }
-    }
-}
